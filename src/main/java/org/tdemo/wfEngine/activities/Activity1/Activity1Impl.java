92482f22ac0fa594d3c86f5fdf4fb73e04be2b20
Manish Rawat <manish@Manishs-MacBook-Air.local>
2024-06-24 17:21:05 +0530

    wip

--- src/main/java/org/tdemo/wfEngine/activities/Activity1/Activity1Impl.java
+++ src/main/java/org/tdemo/wfEngine/activities/Activity1/Activity1Impl.java
@@ -3,13 +3,15 @@ package org.tdemo.wfEngine.activities.Activity1;
 import org.tdemo.wfEngine.activities.Activity1.domain.ProcessRequest;
 import org.tdemo.wfEngine.activities.Activity1.domain.ProcessingOutput;
 
+import java.util.Random;
+
 public class Activity1Impl implements Activity1{
 
     @Override
     public ProcessingOutput startVerification(ProcessRequest request) {
 
         try {
-            Thread.sleep(10000);
+            Thread.sleep(5000 + new Random(System.currentTimeMillis()).nextInt(5000) );
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
